db99f34e8fcad11cb13ab6e893f2f25a29dc55fb
Robert Ely <robertely@gmail.com>
2021-01-06 17:53:34 -0700

    Aw we derping

--- checkers/crazyivan/crazyivan.go
+++ checkers/crazyivan/crazyivan.go
@@ -10,12 +10,12 @@ type Crazyivan struct {
 }
 
 // New - Chonkers
-func New() Crazyivan {
-	return Crazyivan{}
+func New() *Crazyivan {
+	return &Crazyivan{}
 }
 
 // Check - Chonks
-func Check(c Crazyivan) (bool, error) {
+func (c *Crazyivan) Check() (bool, error) {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(2) == 1, nil
 }
--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	ivan "github.com/robertely/fisher/checkers/crazyivan"
 )
 
-// var x Checker
+var x Checker
 
 func main() {
-	x := ivan.New()
+	x = ivan.New()
+
 	x.Name = "Ivan_1"
-	fmt.Println(x.Check()) // No work
+
+	fmt.Println(x.Check())
 
 	fmt.Println(x)
 }
